ea470a0fb98d1337b7a0675f99c120a79486196a
Joao Matos Silva <kappy@acydburne.com.pt>
2015-06-17 14:22:14 +0100

    Added solution files and presentation

---
+++ ImageRichWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ImageRichWebSite.Startup))]
+namespace ImageRichWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
